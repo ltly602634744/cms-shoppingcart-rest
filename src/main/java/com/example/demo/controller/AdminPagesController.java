26603a9c2fdb3033569979753d73d457e931cb64
ltly602634744 <yan.liyan1204@gmail.com>
2020-04-10 12:16:14 -0400

    Get Edit page

--- src/main/java/com/example/demo/controller/AdminPagesController.java
+++ src/main/java/com/example/demo/controller/AdminPagesController.java
@@ -1,11 +1,14 @@
 package com.example.demo.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,6 +38,15 @@ public class AdminPagesController {
 	@ResponseStatus(HttpStatus.CREATED)
 	public Page add(@RequestBody Page page) {
 		return pageRepo.save(page);
+	}
+	
+	@GetMapping("/edit/{id}")
+	public ResponseEntity<Page> edit(@PathVariable int id){
+		
+		Optional<Page> page = pageRepo.findById(id);
+		
+		return new ResponseEntity<>(page.get(), HttpStatus.OK);
+		
 	}
 	
 	
